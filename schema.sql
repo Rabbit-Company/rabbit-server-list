7f8ed9f253bd1fb42b3b0f48051483a699ff3311
Ziga <ziga.zajc007@gmail.com>
2023-03-20 21:26:05 +0100

    Update

--- schema.sql
+++ schema.sql
@@ -8,3 +8,30 @@ CREATE TABLE accounts (
 	created TEXT NOT NULL,
 	accessed TEXT NOT NULL
 );
+
+CREATE TABLE minecraft (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	owner TEXT NOT NULL,
+	name TEXT NOT NULL,
+	ip TEXT NOT NULL,
+	port INTEGER NOT NULL DEFAULT 25565,
+	website TEXT,
+	communication TEXT,
+	version TEXT,
+	categories TEXT,
+	country TEXT,
+	language TEXT,
+	logo TEXT,
+	banner TEXT,
+	description TEXT,
+	players INTEGER NOT NULL DEFAULT 0,
+	players_max INTEGER NOT NULL DEFAULT 0,
+	online INTEGER NOT NULL DEFAULT 0,
+	votes INTEGER NOT NULL DEFAULT 0,
+	votes_total INTEGER NOT NULL DEFAULT 0,
+	votifierIP TEXT NOT NULL,
+	votifierPort INTEGER NOT NULL DEFAULT 8192,
+	votifierToken TEXT NOT NULL,
+	created TEXT NOT NULL,
+	updated TEXT NOT NULL
+);
