59489a65f9e5deee194b4e0ba7aca0a4c9162680
Ziga <ziga.zajc007@gmail.com>
2023-03-21 10:25:22 +0100

    Update

--- src/servers/minecraft.js
+++ src/servers/minecraft.js
@@ -45,6 +45,21 @@ export default class Minecraft{
 		if(data['votifierToken'] !== null && (!Validate.minecraftVotifierToken(data['votifierToken']))) return Errors.getJson(1021);
 
 		if(!(await Utils.authenticate(username, token))) return Errors.getJson(1008);
+
+		let categories = "";
+		for(let i = 0; i < data['categories'].length; i++){
+			categories += data['categories'][i] + ',';
+		}
+		categories = categories.substring(0, categories.length-1);
+
+		try{
+			await Utils.env.DB.prepare("INSERT INTO minecraft(owner, name, ip, port, website, communication, version, categories, country, description, votifierIP, votifierPort, votifierToken, created, updated) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+			.bind(username, data['name'], data['ip'], data['port'], data['website'], data['communication'], data['version'], categories, data['country'], data['description'], data['votifierIP'], data['votifierPort'], data['votifierToken'], Utils.date, Utils.date).run();
+		}catch{
+			return Errors.getJson(1009);
+		}
+
+		return Errors.getJson(0);
 	}
 
 }
