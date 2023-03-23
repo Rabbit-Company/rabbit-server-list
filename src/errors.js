bf3fef456af71b861e092617887a7f5b8c41299a
Ziga <ziga.zajc007@gmail.com>
2023-03-23 18:03:53 +0100

    Changed hashing algorithm

--- src/errors.js
+++ src/errors.js
@@ -5,7 +5,7 @@ export default class Errors{
 		1000: 'Not all required data provided in json format.',
 		1001: 'Username can only contain lowercase characters, numbers and hyphens. It also needs to start with lowercase character and be between 4 and 30 characters long.',
 		1002: 'Invalid email address.',
-		1003: 'Password needs to be hashed with Argon2id. The length of hashed password needs to be 128 characters.',
+		1003: 'Password needs to be hashed with Blake2b. The length of hashed password needs to be 128 characters.',
 		1004: 'Token is invalid. Please login first to get the token.',
 		1005: 'Username is already registered.',
 		1006: 'Basic Authentication is missing.',
