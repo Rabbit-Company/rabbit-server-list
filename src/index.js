fca0490483dddfef0e5020754957bd141e756707
Ziga <ziga.zajc007@gmail.com>
2023-03-29 17:09:20 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -268,14 +268,14 @@ export class MinecraftVoteDO{
 
 			let ipVoteDate = await this.state.storage.get('votedate-ip-' + data['ip']);
 			if(ipVoteDate != null){
-				if((Date.now() - MinecraftVoteDO.voteLimit) > Number(ipVoteDate)){
+				if((Date.now() - MinecraftVoteDO.voteLimit) < Number(ipVoteDate)){
 					return MinecraftVoteDO.jsonResponse({ 'error': 3000, 'info': 'You have already voted today.' });
 				}
 			}
 
 			let usernameVoteDate = await this.state.storage.get('votedate-username-' + data['username']);
 			if(usernameVoteDate != null){
-				if((Date.now() - MinecraftVoteDO.voteLimit) > Number(usernameVoteDate)){
+				if((Date.now() - MinecraftVoteDO.voteLimit) < Number(usernameVoteDate)){
 					return MinecraftVoteDO.jsonResponse({ 'error': 3000, 'info': 'You have already voted today.' });
 				}
 			}
