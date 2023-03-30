cc8a53658cb3b7449fd82048594192024f57d8d8
Ziga <ziga.zajc007@gmail.com>
2023-03-30 20:25:17 +0200

    Update

--- src/dos.js
+++ src/dos.js
@@ -51,7 +51,7 @@ export class MinecraftVoteDO{
 
 		let currentAlarm = await this.state.storage.getAlarm();
 		if(currentAlarm == null){
-			let nextMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1).getTime() + 300_000;
+			let nextMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1).getTime() + 60_000;
       this.state.storage.setAlarm(nextMonth);
     }
 
